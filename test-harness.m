0ef19f8398900482fe22c05b9201668644a4b993
mohitk74 <163093956+mohitk74@users.noreply.github.com>
2024-06-11 15:20:04 +0530

    Create test-harness.m

---
+++ test-harness.m
@@ -0,0 +1,12 @@
+modelName='Sample_Model';
+open_system(modelName);
+% Creating the test harness having source as 'From Workspace' and Sink 'Outport'
+  sltest.harness.create(modelName, 'Name', 'Sample_Model_Harness', 'Source', 'From Workspace', 'Sink', 'Outport');
+% Opening the test Harness model
+sltest.harness.open(modelName,'Sample_Model_Harness');
+harnessName = [modelName,'Sample_Model_Harness'];
+if bdIsLoaded(harnessName)
+    fprintf('Test harness %s is open.\n', harnessName);
+else
+    fprintf('Test harness %s is not open.\n', harnessName);
+end
